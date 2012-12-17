d230aed68c9fdd30bf8175675cfa5a29831a310e
Shawn Allen <shawn@stamen.com>
2012-12-17 14:33:34 -0800

    spreadsheet helper functions

---
+++ d3.sheet.js
@@ -0,0 +1,151 @@
+(function(exports) {
+
+  d3.sheet = {};
+
+  d3.sheet.parse = {};
+
+  var d3_sheet_numberPattern = /^\s*([\d\.\,]+)\s*$/,
+      d3_sheet_numberReplace = function(match, num) {
+        return num.replace(/,/g, "");
+      };
+  d3.sheet.parse.number = function(str) {
+    return +str.replace(d3_sheet_numberPattern, d3_sheet_numberReplace);
+  };
+
+  var d3_sheet_moneyPattern = /^\s*\$([\d\.\,]+)\s*$/,
+      d3_sheet_moneyReplace = function(match, num) {
+        return num.replace(/,/g, "");
+      };
+  d3.sheet.parse.money = function(str) {
+    return +str.replace(d3_sheet_moneyPattern, d3_sheet_moneyReplace);
+  };
+
+  d3.sheet.table = function() {
+    var columns = [],
+        columnsByKey = {};
+
+    function table(rows) {
+      var unpack = d3.sheet.unpack();
+      columns.forEach(function(col) {
+        unpack.column(col.key, col.parse);
+      });
+
+      var unpacked = rows.map(unpack);
+      return {
+        cols: columns,
+        rows: unpacked
+      };
+    }
+
+    table.column = function(key, attrs) {
+      if (arguments.length === 2) {
+        if (!columnsByKey.hasOwnProperty(key)) {
+          columns.push(attrs);
+        }
+        columnsByKey[key] = attrs;
+      } else if (arguments.length === 1) {
+        return columnsByKey[key];
+      } else {
+        return table;
+      }
+    };
+
+    table.columns = function(cols) {
+      if (arguments.length) {
+        columns = cols;
+        columnsByKey = {};
+        columns.forEach(function(col) {
+          columnsByKey[col.key] = col;
+        });
+        return table;
+      } else {
+        return columns;
+      }
+    };
+
+    return table;
+  };
+
+  /**
+   * The spreadsheet "unpacker" returns a function suitable for Array mapping,
+   * which converts the columns (keys) of row objects (from text delimited
+   * files) into objects with the original and parsed values. By default, each
+   * key in a row object is coerced with the native Number() function, and if
+   * the resulting value is not NaN, the numeric value is stored. E.g.:
+   *
+   * var unpack = d3.sheet.unpack();
+   * unpack({foo: "100"}) ->
+   * 
+   * {
+   *   foo: {
+   *     string: "100",
+   *     numeric: true,
+   *     value: 100
+   *   }
+   * }
+   *
+   * You can specify custom parsers for each column:
+   *
+   * var unpack = d3.sheet.unpack()
+   *   .column("amount", d3.sheet.unpack.money)
+   *
+   * unpack({year: "2000", amount: "$5,000.00"}) ->
+   *
+   * {
+   *   year: {
+   *     string: "2000",
+   *     numeric: true,
+   *     value: 2000
+   *   },
+   *   amount: {
+   *     string: "$5,000.00",
+   *     numeric: true,
+   *     value: 5000.0
+   *   }
+   * }
+   */
+  d3.sheet.unpack = function() {
+    var parsers = {};
+
+    function unpack(row) {
+      var out = {};
+      for (var key in row) {
+        var value = row[key],
+            parser = parsers[key] || d3.sheet.parse.number,
+            num = parser(value),
+            numeric = !isNaN(num);
+        out[key] = {
+          string: value,
+          numeric: numeric,
+          value: numeric ? num : value
+        };
+      }
+      return out;
+    }
+
+    unpack.column = function(key, parse) {
+      if (arguments.length === 2) {
+        parsers[key] = parse;
+        return unpack;
+      } else if (arguments.length === 1) {
+        return parsers[key];
+      } else {
+        throw "unpack.column() expects 1 or 2 arguments; got " + arguments.length;
+      }
+    };
+
+    unpack.columns = function(cols) {
+      if (arguments.length) {
+        for (var key in cols) {
+          columns[key] = cols[key];
+        }
+        return unpack;
+      } else {
+        return columns;
+      }
+    };
+
+    return unpack;
+  };
+
+})(this);
